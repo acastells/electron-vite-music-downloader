ebdbf0f2c9dce51cb01fe6d9a8037c77c5d5934f
acastells <36441239+acastells@users.noreply.github.com>
2024-01-25 18:34:44 +0100

    fix the unzip setup script bug for ffmpeg

--- src/main/setupFfmpeg.ts
+++ src/main/setupFfmpeg.ts
@@ -1,6 +1,9 @@
 import { BrowserWindow, app, ipcMain } from "electron";
 import path from "path";
 const fs = require("fs");
+const decompress = require("decompress");
+
+export const versionFFmpeg = "ffmpeg-master-latest-win64-lgpl-shared";
 
 export const setupFFmpeg = () => {
 	app.whenReady().then(() => {
@@ -8,13 +11,15 @@ export const setupFFmpeg = () => {
 	});
 
 	let ffmpegDownloadOptions = {
+		filename: "ffmpeg.zip",
+		directory: path.join(app.getPath("appData"), "Music Downloader"),
 		zipPath: path.join(app.getPath("appData"), "Music Downloader", "ffmpeg.zip"),
 		folderPath: path.join(app.getPath("appData"), "Music Downloader", "ffmpeg"),
 		binPath: path.join(
 			app.getPath("appData"),
 			"Music Downloader",
 			"ffmpeg",
-			"ffmpeg-master-latest-win64-lgpl-shared",
+			versionFFmpeg,
 			"bin",
 			"ffmpeg.exe"
 		),
@@ -27,14 +32,14 @@ export const setupFFmpeg = () => {
 		const binPathExists = fs.existsSync(ffmpegDownloadOptions.binPath);
 
 		if (zipPathExists && binPathExists) {
-			console.log("ffmpeg exists and is unzipped, no operation...");
+			console.log("ffmpeg.exists and is unzipped, no operation...");
 		} else if (zipPathExists && !binPathExists) {
-			console.log("ffmpeg zip exists, unzipping...");
+			console.log("ffmpeg.zip exists, unzipping...");
 			unzipFfmpeg();
 		} else {
 			try {
 				const url = "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest";
-				const finalUrl = `${url}/ffmpeg-master-latest-win64-lgpl-shared.zip`;
+				const finalUrl = `${url}/${versionFFmpeg}`;
 				const win = BrowserWindow.getFocusedWindow();
 				ffmpegDownloadOptions.onStarted = () => {};
 				ffmpegDownloadOptions.onCompleted = unzipFfmpeg;
@@ -47,16 +52,12 @@ export const setupFFmpeg = () => {
 	};
 
 	const unzipFfmpeg = () => {
-		const { createReadStream } = require("fs");
-		const unzipper = require("unzipper");
-
-		createReadStream(ffmpegDownloadOptions.zipPath)
-			.pipe(unzipper.Extract({ path: ffmpegDownloadOptions.folderPath }))
-			.on("finish", () => {
-				console.log("Unzipped successfully!");
+		decompress(ffmpegDownloadOptions.zipPath, ffmpegDownloadOptions.folderPath)
+			.then(() => {
+				console.log("unzipped");
 			})
-			.on("error", (err) => {
-				console.error("Error while unzipping:", err);
+			.catch((error) => {
+				console.log(error);
 			});
 	};
 };
--- src/main/setupYtdlp.ts
+++ src/main/setupYtdlp.ts
@@ -1,10 +1,26 @@
 import { app, ipcMain } from "electron";
 import path from "path";
-const fs = require("fs");
-const YTDlpWrap = require("yt-dlp-wrap").default; // TS version does not work
+import { versionFFmpeg } from "./setupFfmpeg";
+const YTDlpWrap = require("yt-dlp-wrap").default; // TS version does not work // https://github.com/foxesdocode/yt-dlp-wrap
 
 export const setupYtdlp = () => {
-	const binaryPath = path.join(app.getPath("appData"), "Music Downloader", "yt-dlp.exe");
+	const binaryPath = path.join(
+		app.getPath("appData"),
+		"Music Downloader",
+		"ffmpeg",
+		versionFFmpeg,
+		"bin",
+		"yt-dlp.exe"
+	);
+	const downloadPath = app.getPath("desktop");
+	const ffmpegPath = path.join(
+		app.getPath("appData"),
+		"Music Downloader",
+		"ffmpeg",
+		versionFFmpeg,
+		"bin",
+		"ffmpeg.exe"
+	);
 
 	app.whenReady().then(() => {
 		ipcMain.on("download_dlp", setupDlp);
@@ -17,8 +33,34 @@ export const setupYtdlp = () => {
 		});
 	};
 
-	const downloadTrack = () => {
+	const downloadTrack = (_event, listener) => {
+		const { name, type } = listener;
+		console.log(name, type);
+
 		const ytDlpWrap = new YTDlpWrap(binaryPath);
-		console.log(ytDlpWrap);
+
+		let ytDlpEventEmitter = ytDlpWrap
+			.exec([
+				"https://www.youtube.com/watch?v=aqz-KE-bpKQ",
+				"-o",
+				"%(title)s - %(artist)s.%(ext)s",
+				"-P",
+				downloadPath,
+				"--extract-audio",
+				"--audio-format",
+				"mp3",
+				"--audio-quality",
+				"320",
+				"--ffmpeg-location",
+				ffmpegPath,
+				"--default-search",
+				"ytsearch:",
+			])
+			.on("progress", (progress) => console.log(progress.percent))
+			.on("ytDlpEvent", (eventType, eventData) => console.log("hey", eventType, eventData))
+			.on("error", (error) => console.error(error))
+			.on("close", () => console.log("all done"));
+
+		console.log(ytDlpEventEmitter.ytDlpProcess.pid);
 	};
 };
